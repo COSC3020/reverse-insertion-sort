8c360b54a3cfd2d9c16af7e19fa9cdd7f7d470fa
Lars Kotthoff <larsko@uwyo.edu>
2023-08-07 11:16:54 -0600

    testing and instructions

--- code.test.js
+++ code.test.js
@@ -1,14 +1,15 @@
+const fs = require('fs');
 const jsc = require('jsverify');
 
-test("sorts correctly", () => {
-    const testSort =
-        jsc.forall("array nat", function(arr) {
-            var a1 = JSON.parse(JSON.stringify(arr));
-            var a2 = JSON.parse(JSON.stringify(arr));
-            return JSON.stringify(insertionSortReverse(a1)) ==
-                JSON.stringify(a2.sort(function(a, b)
-                    { return a - b; }));
-        });
+eval(fs.readFileSync('code.js')+'');
 
-    jsc.check(testSort);
-});
+const testSort =
+    jsc.forall("array nat", function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = JSON.parse(JSON.stringify(arr));
+        return JSON.stringify(insertionSortReverse(a1)) ==
+            JSON.stringify(a2.sort(function(a, b)
+                { return a - b; }));
+    });
+
+jsc.check(testSort);
