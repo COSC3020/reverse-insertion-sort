83410ba3d947a4a034705089cb94aa504aef1a45
tyson <tlimato@uwyo.edu>
2024-01-23 20:15:10 -0700

    got rid of trying to use splice

--- code.js
+++ code.js
@@ -3,22 +3,36 @@
 
 // only differnece is this iterates right to left.
 function insertionSortReverse(array) {
-
+    console.log(array);
     // Must also include the 0 index
-    for (var i = length - 1; i >= 0; i-- ){
+    for (var i = array.length - 1; i >= 0; i-- ){
         var val = array[i];
         // A binary search would be effective here as mentioned in lecture
         // J represents the appropriate INDEX for the replacement
         var j = binary_search(array, val, 0, i); // are moving window shifts to the left
-
+        // README: I read up on the splice operator because it was mentioned
+        //         In class and I'm in too deep now with trying to make this fast
+        //         Honestly, it is very nice for readability and I assumed faster than
+        //         what would otherwise be a for loop. Unfortunately I couldn't get it to work
+        //         without skipping the last index.
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
         // inserts at index j with val, and shifts other values appropriately
-        // Doesn't overite the index specified.
-        array.splice(j,0,val)
+        // Doesn't ovewrite the index specified.
+        // array.splice(j,0,val)
         // remove original index of the value 
         // which was shifted write when inserting it in the correct possition.
-        array.splice(i+1,1)
+        // array.splice(i+1,1)
+
+        // Shift elements to the right, excluding the current value
+        for (var k = i; k > j; k--) {
+            array[k] = array[k - 1];
+        }
+
+        // Insert the value at index j
+        array[j] = val;
     }
+    console.log("sorted: ", array)
     return array;
 
 }
@@ -26,10 +40,10 @@ function insertionSortReverse(array) {
 
 // For sake of clarity let's use a helper function
 function binary_search(arr, val, beg, end){
-    var middle = beg + end >> 1;  // bit shift right for faster divide by 2
+    var middle = (beg + end) >> 1;  // bit shift right for faster divide by 2
     // base case
     if (beg > end){
-        return start;
+        return beg;
     }
     // RECURSIVE CASE
     // if middle is the val, return middle
@@ -39,12 +53,12 @@ function binary_search(arr, val, beg, end){
     else if (arr[middle] < val){
         // when middle is less than the target value:
         //  the search range is updated to right half of the current range
-        binary_search(arr, val, mid +1, end);
+        return binary_search(arr, val, middle + 1, end);
     }
     else{
         // when middle element is greater than the target value:
         //  the search range is updated to left half of the current range
-        binary_search(arr, val, start, mid -1);
+        return binary_search(arr, val, beg, middle - 1);
     } 
 
 }
