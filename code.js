4d6b5a5f896674c10542a8c1e1762b5caa74f155
tyson <tlimato@uwyo.edu>
2024-01-23 18:46:45 -0700

    Beggining of binary search

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function insertionSortReverse(array) {
+    // Must also include the 0 index
+    for (var i = length - 1; i >= 0; i-- ){
+        var val = arr[i];
+        var j = i -1; // are moving window shifts to the left
+        // A binary search would be effective here as mentioned in lecture
+
+    }
     return array;
+}
+
+
+// For sake of clarity let's use a helper function
+function binary_search(arr, val, beg, end){
+    var middle = beg + end >> 1;  // bit shift right for faster divide by 2
+
 }
