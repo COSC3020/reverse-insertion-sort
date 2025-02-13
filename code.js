1549874006391ed468aae440500b264130dcfd30
Lars Kotthoff <lars@larsko.org>
2025-02-13 13:19:37 -0700

    fix eslint action

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
-// eslint-disable-next-line no-unused-varsfunction insertionSortReverse(array) {
+// eslint-disable-next-line no-unused-vars
+function insertionSortReverse(array) {
     return array;
 }
